3ebf4dc01a549f5d9f056ec267d2a0eedae65881
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-09-24 19:41:12 -0400

    my python

---
+++ print_names.py
@@ -0,0 +1,9 @@
+import random
+
+list1 = ["Rayat", "William", "Michelle", "Lucas", "Ivan"];
+list2 = ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"];
+
+for i in list1:
+    print(i)
+for i in list2:
+    print(i)
