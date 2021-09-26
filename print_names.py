779d1a8059c37ba2dbf7b19faa937ae5f83ce1b3
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-09-26 14:56:05 -0400

    removed other file

--- print_names.py
+++
@@ -1,9 +0,0 @@
-import random
-
-list1 = ["Rayat", "William", "Michelle", "Lucas", "Ivan"];
-list2 = ["Yoonah", "Joshua", "Alif", "Josephine", "Andrew"];
-
-for i in list1:
-    print(i)
-for i in list2:
-    print(i)
