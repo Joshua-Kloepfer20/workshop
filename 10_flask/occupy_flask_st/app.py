607fdfaf40effb2f7ee09ddf72d3b073ff3d6f56
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-10-05 19:29:20 -0400

    work

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -4,24 +4,40 @@ SoftDev
 K10 -- To review several iterations of a flask app, and to make our own flask app that displays the results of the random occupation picker code we wrote.
 2021-10-04
 '''
+
 import csv
 import random
 from flask import Flask
-app = Flask(__name__)
+
+# Declaration of dictionary to store CSV File contents
 finalDict = {}
-def randomJob(reader):
-        x = random.randint(0, 1000)
-        y = 0
-        for i in reader:
-                y += reader[i] * 10
-                if (y > x):
-                        return("The Best Team -- Alif Abdullah, Andrew Juang, Joshua Kloepfer<br><br>List of occupations: <br>" + str(list(reader.keys())[:len(list(reader.keys()))-1]) + "<br><br>Chosen Occupation: " + str(i))
-                        break
-with open('occupations.csv', newline='') as csvfile:
+# Create instance of flask app
+app = Flask(__name__)
+
+# Read in CSV file
+def fileReader():
+    with open('occupations.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-                finalDict[row['Job Class']] = float(row['Percentage'])
+            finalDict[row['Job Class']] = float(row['Percentage'])
+
+# Choose random job from dictionary
+def randomJob(reader):
+    x = random.randint(0, 1000)
+    y = 0
+    for i in reader:
+        y += reader[i] * 10
+        if y > x:
+            return i
+            break
+
+# Running the flask app with displayApp function
 @app.route("/")
-def hello_world():
-    return(str(randomJob(finalDict)))
+def displayApp():
+    fileReader()
+    return ("The Best Team -- Alif Abdullah, Andrew Juang, Joshua Kloepfer <br><br>List of occupations: <br>" +
+        str(list(finalDict.keys())[:len(list(finalDict.keys()))-1]) +
+        "<br><br>Chosen Occupation: " + str(randomJob(finalDict)))
+
+app.debug = True
 app.run()
