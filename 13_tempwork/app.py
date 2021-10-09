82a862b9cec3c5a07bdba2e90ce36f4ed510960f
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-10-09 12:37:51 -0400

    some work

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -8,21 +8,21 @@ def hello_world():
     return "No hablo queso!"
 
 @app.route("/occupyflaskst")
-def test_tmplt():
-    return render_template('tablified.html', foo="Hello")
-def randomJob(reader):
-       finalDict = {}
-       with open('data/occupations.csv', newline='') as csvfile:
+def randomJob():
+        finalDict = {}
+        with open('data/occupations.csv', newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                        finalDict[row['Job Class']] = float(row['Percentage'])
+                    finalDict[row['Job Class']] = float(row['Percentage'])
         x = random.randint(0, 1000)
         y = 0
-        for i in reader:
-                y += reader[i] * 10
+        for i in finalDict:
+                y += finalDict[i] * 10
                 if (y > x):
-                        return ("The Best Team -- Alif Abdullah, Andrew Juang, Joshua Kloepfer <br><br>List of occupations: <br>" + finalDict+ "<br><br>Chosen Occupation: " + str(finalDict))
+                        return template() + ("Team name -- Joshua Kloepfer, Mark Zhu <br><br>List of occupations: <br>" + str(list(finalDict)) + "<br><br>Chosen Occupation: " + str(i))
                         break
+def template():
+    return render_template('tablified.html',foo="Team name")
 if __name__ == "__main__":
     app.debug = True
     app.run()
