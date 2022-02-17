5bcee37611e75631007112156d01a880ffbdf49f
Joshua Kloepfer <jkloepfer20@stuy.edu>
2022-02-17 08:41:55 -0500

    work

--- 32_js-anim/animate.js
+++ 32_js-anim/animate.js
@@ -12,7 +12,7 @@
 var c = document.getElementById("playground");// GET CANVAS
 var dotButton = document.getElementById("buttonCircle");// GET DOT BUTTON
 var stopButton = document.getElementById("buttonStop");// GET STOP BUTTON
-
+var screenButton = document.getElementById("buttonScreen");
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d");// YOUR CODE HERE
 
@@ -36,6 +36,7 @@ var growing = true; //is the circle growing?
 //var drawDot = function() {
 var drawDot = () => {
   console.log("drawDot invoked...");
+  window.cancelAnimationFrame(requestID);
 
   clear();
   if (growing == true && radius >= 250) { //250 = max radius of canvas (500/2)
@@ -85,8 +86,37 @@ var stopIt = () => {
     window.cancelAnimationFrame()
   */
 };
+var velx, vely, x, y;
+x = 250;
+y = 250;
+velx = 1;
+vely = 1;
+var screen = () => {
+  window.cancelAnimationFrame(requestID);
+  clear()
+  console.log("screen saver")
+  if (x + 50 >= 500) {
+    velx = -1;
+  }
+  if (x - 50 <= 0) {
+    velx = 1
+  }
+  if (y + 50 >= 500) {
+    vely = -1;
+  }
+  if (y - 50 <= 0) {
+    vely = 1
+  }
+  x += velx;
+  y += vely;
+  ctx.beginPath();
+  ctx.arc(x, y, 50, 0, 2*Math.PI); //summon a circle!: (x,y,radius,startAngle,endAngle)
+  ctx.fill();
+  requestID = window.requestAnimationFrame(screen);
+};
 
 
-dotButton.addEventListener( "click", function() {if (requestID == null) {drawDot()}} );
+dotButton.addEventListener( "click", drawDot );
 //if requestID != null, then it means the circle is growing / animating --> even if user clicks button, the fxn drawDot won't be called
 stopButton.addEventListener( "click",  stopIt );
+screenButton.addEventListener("click", function() {velx = 1; vely = 1; x = Math.round(Math.random() * 400 + 50); y = Math.round(Math.random() * 400 + 50); screen();});
